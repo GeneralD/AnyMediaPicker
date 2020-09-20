9960ca37fc1ccfa82dcae3f830e4062a3c1bc99a
GeneralD <yumejustice@gmail.com>
2020-09-21 00:37:54 +0900

    small fix

--- AnyMediaPicker/Sources/Extensions/UITableView+RxCellsDataSource.swift
+++ AnyMediaPicker/Sources/Extensions/UITableView+RxCellsDataSource.swift
@@ -14,21 +14,18 @@ import Reusable
 
 public extension Reactive where Base: UITableView {
 	
-	func reloadCells<S: Sequence, Cell: UITableViewCell, O: ObservableType>(_: Cell.Type)
-		-> (_ _: O)
-		-> Disposable
-		where O.Element == S, Cell: Reusable & Configurable, Cell.Model == S.Iterator.Element {
-			return { source in
-				return source
-					.map { $0.map { $0 } }
-					.map { SectionModel<(), Cell.Model>(model: (), items: $0) }
-					.map { [$0] }
-					.bind(to: self.items(dataSource: RxTableViewSectionedReloadDataSource<SectionModel<(), Cell.Model>>(configureCell: { _, tableView, indexPath, item in
-						let cell = tableView.dequeueReusableCell(withIdentifier: Cell.reuseIdentifier, for: indexPath) as! Cell
-						cell.configure(with: item)
-						return cell
-					})))
-			}
+	func reloadCells<S: Sequence, Cell: UITableViewCell, O: ObservableType>(_: Cell.Type) -> (_ _: O) -> Disposable where O.Element == S, Cell: Reusable & Configurable, Cell.Model == S.Iterator.Element {
+	{ source in
+		source
+			.map(Array.init) // sequence to array
+			.map { SectionModel<(), Cell.Model>(model: (), items: $0) } // cell models to a section model
+			.map { [$0] } // as single section array
+			.bind(to: self.items(dataSource: RxTableViewSectionedReloadDataSource<SectionModel<(), Cell.Model>>(configureCell: { _, tableView, indexPath, item in
+				let cell = tableView.dequeueReusableCell(withIdentifier: Cell.reuseIdentifier, for: indexPath) as! Cell
+				cell.configure(with: item)
+				return cell
+			})))
+		}
 	}
 }
 
