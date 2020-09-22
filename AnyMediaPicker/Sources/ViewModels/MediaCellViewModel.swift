c7390924eac1970c0637884ce875f3d6e8dcf6be
GeneralD <yumejustice@gmail.com>
2020-09-22 18:03:47 +0900

    bind cell and its vm

--- AnyMediaPicker/Sources/ViewModels/MediaCellViewModel.swift
+++ AnyMediaPicker/Sources/ViewModels/MediaCellViewModel.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 import RxSwift
 import RxRelay
 
@@ -15,7 +16,8 @@ protocol MediaCellViewModelInput {
 }
 
 protocol MediaCellViewModelOutput {
-	
+	var thumbnail: Observable<UIImage?> { get }
+	var title: Observable<String?> { get }
 }
 
 final class MediaCellViewModel: MediaCellViewModelInput, MediaCellViewModelOutput {
@@ -23,11 +25,14 @@ final class MediaCellViewModel: MediaCellViewModelInput, MediaCellViewModelOutpu
 	// MARK: Inputs
 	
 	// MARK: Outputs
+	let thumbnail: Observable<UIImage?>
+	let title: Observable<String?>
 	
 	private let disposeBag = DisposeBag()
 	
 	init(model: MediaCellModel) {
-		
+		thumbnail = .just(model.image)
+		title = .just(model.title)
 	}
 }
 
--- AnyMediaPicker/Sources/Views/MediaCellView.swift
+++ AnyMediaPicker/Sources/Views/MediaCellView.swift
@@ -14,20 +14,20 @@ import RxBinding
 import Reusable
 
 class MediaCellView: UITableViewCell {
-
+	
 	private typealias Input = MediaCellViewModelInput
 	private typealias Output = MediaCellViewModelOutput
-
+	
 	@IBOutlet private weak var thumbnailImageView: UIImageView!
 	@IBOutlet private weak var titleLabel: UILabel!
-
+	
 	private var input: Input!
 	private var output: Output!
 	private var disposeBag: DisposeBag!
-
+	
 	override func awakeFromNib() {
 		super.awakeFromNib()
-
+		
 		
 	}
 }
@@ -35,15 +35,18 @@ class MediaCellView: UITableViewCell {
 extension MediaCellView: NibLoadable, Reusable {}
 
 extension MediaCellView: Configurable {
-
+	
 	func configure(with model: MediaCellModel) {
 		let viewModel = MediaCellViewModel(model: model)
 		input = viewModel
 		output = viewModel
-
+		
 		// Unbind all
 		disposeBag = .init()
 		
 		// Bind again
+		disposeBag ~
+			output.thumbnail ~> thumbnailImageView.rx.image ~
+			output.title ~> titleLabel.rx.text
 	}
 }
