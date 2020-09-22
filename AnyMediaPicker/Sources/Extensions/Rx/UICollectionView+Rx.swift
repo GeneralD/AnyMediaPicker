98dec500a5b800d5bdd546aca68425a2c70afb38
GeneralD <yumejustice@gmail.com>
2020-09-22 19:51:17 +0900

    refactor

--- AnyMediaPicker/Sources/Extensions/Rx/UICollectionView+Rx.swift
+++ AnyMediaPicker/Sources/Extensions/Rx/UICollectionView+Rx.swift
@@ -14,16 +14,19 @@ import Reusable
 
 public extension Reactive where Base: UICollectionView {
 	
-	func reloadCells<S: Sequence, Cell: UICollectionViewCell, O: ObservableType>(_: Cell.Type) -> (_ _: O) -> Disposable where O.Element == S, Cell: Reusable & Configurable, Cell.Model == S.Iterator.Element {
+	func reloadCells<S: Sequence, Cell: UICollectionViewCell, O: ObservableType>(_: Cell.Type, canMove: Bool = false) -> (_ _: O) -> Disposable where O.Element == S, Cell: Reusable & Configurable, Cell.Model == S.Iterator.Element {
 	{ source in
 		source
 			.map(Array.init) // sequence to array
 			.map { SectionModel<(), Cell.Model>(model: (), items: $0) } // cell models to a section model
 			.map { [$0] } // as single section array
-			.bind(to: self.items(dataSource: RxCollectionViewSectionedReloadDataSource<SectionModel<(), Cell.Model>>(configureCell: { _, collectionView, indexPath, item in
-				let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Cell.reuseIdentifier, for: indexPath) as! Cell
-				cell.configure(with: item)
-				return cell
+			.bind(to: self.items(dataSource: RxCollectionViewSectionedReloadDataSource<SectionModel<(), Cell.Model>>(
+				configureCell: { _, collectionView, indexPath, item in
+					let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Cell.reuseIdentifier, for: indexPath) as! Cell
+					cell.configure(with: item)
+					return cell
+			}, canMoveItemAtIndexPath: {
+				_, _ in canMove
 			})))
 		}
 	}
